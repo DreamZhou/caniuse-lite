6272d6a11c891b7554d47d7ba7b075466a97ede9
DreamZhou <971300852@qq.com>
2023-01-28 23:51:55 +0800

    add region-data

---
+++ data/region-data.js
@@ -0,0 +1,485 @@
+import AD from "./regions/AD.js";
+import AE from "./regions/AE.js";
+import AF from "./regions/AF.js";
+import AG from "./regions/AG.js";
+import AI from "./regions/AI.js";
+import AL from "./regions/AL.js";
+import alt_af from "./regions/alt-af.js";
+import alt_an from "./regions/alt-an.js";
+import alt_as from "./regions/alt-as.js";
+import alt_eu from "./regions/alt-eu.js";
+import alt_na from "./regions/alt-na.js";
+import alt_oc from "./regions/alt-oc.js";
+import alt_sa from "./regions/alt-sa.js";
+import alt_ww from "./regions/alt-ww.js";
+import AM from "./regions/AM.js";
+import AO from "./regions/AO.js";
+import AR from "./regions/AR.js";
+import AS from "./regions/AS.js";
+import AT from "./regions/AT.js";
+import AU from "./regions/AU.js";
+import AW from "./regions/AW.js";
+import AX from "./regions/AX.js";
+import AZ from "./regions/AZ.js";
+import BA from "./regions/BA.js";
+import BB from "./regions/BB.js";
+import BD from "./regions/BD.js";
+import BE from "./regions/BE.js";
+import BF from "./regions/BF.js";
+import BG from "./regions/BG.js";
+import BH from "./regions/BH.js";
+import BI from "./regions/BI.js";
+import BJ from "./regions/BJ.js";
+import BM from "./regions/BM.js";
+import BN from "./regions/BN.js";
+import BO from "./regions/BO.js";
+import BR from "./regions/BR.js";
+import BS from "./regions/BS.js";
+import BT from "./regions/BT.js";
+import BW from "./regions/BW.js";
+import BY from "./regions/BY.js";
+import BZ from "./regions/BZ.js";
+import CA from "./regions/CA.js";
+import CD from "./regions/CD.js";
+import CF from "./regions/CF.js";
+import CG from "./regions/CG.js";
+import CH from "./regions/CH.js";
+import CI from "./regions/CI.js";
+import CK from "./regions/CK.js";
+import CL from "./regions/CL.js";
+import CM from "./regions/CM.js";
+import CN from "./regions/CN.js";
+import CO from "./regions/CO.js";
+import CR from "./regions/CR.js";
+import CU from "./regions/CU.js";
+import CV from "./regions/CV.js";
+import CX from "./regions/CX.js";
+import CY from "./regions/CY.js";
+import CZ from "./regions/CZ.js";
+import DE from "./regions/DE.js";
+import DJ from "./regions/DJ.js";
+import DK from "./regions/DK.js";
+import DM from "./regions/DM.js";
+import DO from "./regions/DO.js";
+import DZ from "./regions/DZ.js";
+import EC from "./regions/EC.js";
+import EE from "./regions/EE.js";
+import EG from "./regions/EG.js";
+import ER from "./regions/ER.js";
+import ES from "./regions/ES.js";
+import ET from "./regions/ET.js";
+import FI from "./regions/FI.js";
+import FJ from "./regions/FJ.js";
+import FK from "./regions/FK.js";
+import FM from "./regions/FM.js";
+import FO from "./regions/FO.js";
+import FR from "./regions/FR.js";
+import GA from "./regions/GA.js";
+import GB from "./regions/GB.js";
+import GD from "./regions/GD.js";
+import GE from "./regions/GE.js";
+import GF from "./regions/GF.js";
+import GG from "./regions/GG.js";
+import GH from "./regions/GH.js";
+import GI from "./regions/GI.js";
+import GL from "./regions/GL.js";
+import GM from "./regions/GM.js";
+import GN from "./regions/GN.js";
+import GP from "./regions/GP.js";
+import GQ from "./regions/GQ.js";
+import GR from "./regions/GR.js";
+import GT from "./regions/GT.js";
+import GU from "./regions/GU.js";
+import GW from "./regions/GW.js";
+import GY from "./regions/GY.js";
+import HK from "./regions/HK.js";
+import HN from "./regions/HN.js";
+import HR from "./regions/HR.js";
+import HT from "./regions/HT.js";
+import HU from "./regions/HU.js";
+import ID from "./regions/ID.js";
+import IE from "./regions/IE.js";
+import IL from "./regions/IL.js";
+import IM from "./regions/IM.js";
+import IN from "./regions/IN.js";
+import IQ from "./regions/IQ.js";
+import IR from "./regions/IR.js";
+import IS from "./regions/IS.js";
+import IT from "./regions/IT.js";
+import JE from "./regions/JE.js";
+import JM from "./regions/JM.js";
+import JO from "./regions/JO.js";
+import JP from "./regions/JP.js";
+import KE from "./regions/KE.js";
+import KG from "./regions/KG.js";
+import KH from "./regions/KH.js";
+import KI from "./regions/KI.js";
+import KM from "./regions/KM.js";
+import KN from "./regions/KN.js";
+import KP from "./regions/KP.js";
+import KR from "./regions/KR.js";
+import KW from "./regions/KW.js";
+import KY from "./regions/KY.js";
+import KZ from "./regions/KZ.js";
+import LA from "./regions/LA.js";
+import LB from "./regions/LB.js";
+import LC from "./regions/LC.js";
+import LI from "./regions/LI.js";
+import LK from "./regions/LK.js";
+import LR from "./regions/LR.js";
+import LS from "./regions/LS.js";
+import LT from "./regions/LT.js";
+import LU from "./regions/LU.js";
+import LV from "./regions/LV.js";
+import LY from "./regions/LY.js";
+import MA from "./regions/MA.js";
+import MC from "./regions/MC.js";
+import MD from "./regions/MD.js";
+import ME from "./regions/ME.js";
+import MG from "./regions/MG.js";
+import MH from "./regions/MH.js";
+import MK from "./regions/MK.js";
+import ML from "./regions/ML.js";
+import MM from "./regions/MM.js";
+import MN from "./regions/MN.js";
+import MO from "./regions/MO.js";
+import MP from "./regions/MP.js";
+import MQ from "./regions/MQ.js";
+import MR from "./regions/MR.js";
+import MS from "./regions/MS.js";
+import MT from "./regions/MT.js";
+import MU from "./regions/MU.js";
+import MV from "./regions/MV.js";
+import MW from "./regions/MW.js";
+import MX from "./regions/MX.js";
+import MY from "./regions/MY.js";
+import MZ from "./regions/MZ.js";
+import NA from "./regions/NA.js";
+import NC from "./regions/NC.js";
+import NE from "./regions/NE.js";
+import NF from "./regions/NF.js";
+import NG from "./regions/NG.js";
+import NI from "./regions/NI.js";
+import NL from "./regions/NL.js";
+import NO from "./regions/NO.js";
+import NP from "./regions/NP.js";
+import NR from "./regions/NR.js";
+import NU from "./regions/NU.js";
+import NZ from "./regions/NZ.js";
+import OM from "./regions/OM.js";
+import PA from "./regions/PA.js";
+import PE from "./regions/PE.js";
+import PF from "./regions/PF.js";
+import PG from "./regions/PG.js";
+import PH from "./regions/PH.js";
+import PK from "./regions/PK.js";
+import PL from "./regions/PL.js";
+import PM from "./regions/PM.js";
+import PN from "./regions/PN.js";
+import PR from "./regions/PR.js";
+import PS from "./regions/PS.js";
+import PT from "./regions/PT.js";
+import PW from "./regions/PW.js";
+import PY from "./regions/PY.js";
+import QA from "./regions/QA.js";
+import RE from "./regions/RE.js";
+import RO from "./regions/RO.js";
+import RS from "./regions/RS.js";
+import RU from "./regions/RU.js";
+import RW from "./regions/RW.js";
+import SA from "./regions/SA.js";
+import SB from "./regions/SB.js";
+import SC from "./regions/SC.js";
+import SD from "./regions/SD.js";
+import SE from "./regions/SE.js";
+import SG from "./regions/SG.js";
+import SH from "./regions/SH.js";
+import SI from "./regions/SI.js";
+import SK from "./regions/SK.js";
+import SL from "./regions/SL.js";
+import SM from "./regions/SM.js";
+import SN from "./regions/SN.js";
+import SO from "./regions/SO.js";
+import SR from "./regions/SR.js";
+import ST from "./regions/ST.js";
+import SV from "./regions/SV.js";
+import SY from "./regions/SY.js";
+import SZ from "./regions/SZ.js";
+import TC from "./regions/TC.js";
+import TD from "./regions/TD.js";
+import TG from "./regions/TG.js";
+import TH from "./regions/TH.js";
+import TJ from "./regions/TJ.js";
+import TK from "./regions/TK.js";
+import TL from "./regions/TL.js";
+import TM from "./regions/TM.js";
+import TN from "./regions/TN.js";
+import TO from "./regions/TO.js";
+import TR from "./regions/TR.js";
+import TT from "./regions/TT.js";
+import TV from "./regions/TV.js";
+import TW from "./regions/TW.js";
+import TZ from "./regions/TZ.js";
+import UA from "./regions/UA.js";
+import UG from "./regions/UG.js";
+import US from "./regions/US.js";
+import UY from "./regions/UY.js";
+import UZ from "./regions/UZ.js";
+import VA from "./regions/VA.js";
+import VC from "./regions/VC.js";
+import VE from "./regions/VE.js";
+import VG from "./regions/VG.js";
+import VI from "./regions/VI.js";
+import VN from "./regions/VN.js";
+import VU from "./regions/VU.js";
+import WF from "./regions/WF.js";
+import WS from "./regions/WS.js";
+import YE from "./regions/YE.js";
+import YT from "./regions/YT.js";
+import ZA from "./regions/ZA.js";
+import ZM from "./regions/ZM.js";
+import ZW from "./regions/ZW.js";
+
+export default {
+    AD,
+    AE,
+    AF,
+    AG,
+    AI,
+    AL,
+    "alt-af": alt_af,
+    "alt-an": alt_an,
+    "alt-as": alt_as,
+    "alt-eu": alt_eu,
+    "alt-na": alt_na,
+    "alt-oc": alt_oc,
+    "alt-sa": alt_sa,
+    "alt-ww": alt_ww,
+    AM,
+    AO,
+    AR,
+    AS,
+    AT,
+    AU,
+    AW,
+    AX,
+    AZ,
+    BA,
+    BB,
+    BD,
+    BE,
+    BF,
+    BG,
+    BH,
+    BI,
+    BJ,
+    BM,
+    BN,
+    BO,
+    BR,
+    BS,
+    BT,
+    BW,
+    BY,
+    BZ,
+    CA,
+    CD,
+    CF,
+    CG,
+    CH,
+    CI,
+    CK,
+    CL,
+    CM,
+    CN,
+    CO,
+    CR,
+    CU,
+    CV,
+    CX,
+    CY,
+    CZ,
+    DE,
+    DJ,
+    DK,
+    DM,
+    DO,
+    DZ,
+    EC,
+    EE,
+    EG,
+    ER,
+    ES,
+    ET,
+    FI,
+    FJ,
+    FK,
+    FM,
+    FO,
+    FR,
+    GA,
+    GB,
+    GD,
+    GE,
+    GF,
+    GG,
+    GH,
+    GI,
+    GL,
+    GM,
+    GN,
+    GP,
+    GQ,
+    GR,
+    GT,
+    GU,
+    GW,
+    GY,
+    HK,
+    HN,
+    HR,
+    HT,
+    HU,
+    ID,
+    IE,
+    IL,
+    IM,
+    IN,
+    IQ,
+    IR,
+    IS,
+    IT,
+    JE,
+    JM,
+    JO,
+    JP,
+    KE,
+    KG,
+    KH,
+    KI,
+    KM,
+    KN,
+    KP,
+    KR,
+    KW,
+    KY,
+    KZ,
+    LA,
+    LB,
+    LC,
+    LI,
+    LK,
+    LR,
+    LS,
+    LT,
+    LU,
+    LV,
+    LY,
+    MA,
+    MC,
+    MD,
+    ME,
+    MG,
+    MH,
+    MK,
+    ML,
+    MM,
+    MN,
+    MO,
+    MP,
+    MQ,
+    MR,
+    MS,
+    MT,
+    MU,
+    MV,
+    MW,
+    MX,
+    MY,
+    MZ,
+    NA,
+    NC,
+    NE,
+    NF,
+    NG,
+    NI,
+    NL,
+    NO,
+    NP,
+    NR,
+    NU,
+    NZ,
+    OM,
+    PA,
+    PE,
+    PF,
+    PG,
+    PH,
+    PK,
+    PL,
+    PM,
+    PN,
+    PR,
+    PS,
+    PT,
+    PW,
+    PY,
+    QA,
+    RE,
+    RO,
+    RS,
+    RU,
+    RW,
+    SA,
+    SB,
+    SC,
+    SD,
+    SE,
+    SG,
+    SH,
+    SI,
+    SK,
+    SL,
+    SM,
+    SN,
+    SO,
+    SR,
+    ST,
+    SV,
+    SY,
+    SZ,
+    TC,
+    TD,
+    TG,
+    TH,
+    TJ,
+    TK,
+    TL,
+    TM,
+    TN,
+    TO,
+    TR,
+    TT,
+    TV,
+    TW,
+    TZ,
+    UA,
+    UG,
+    US,
+    UY,
+    UZ,
+    VA,
+    VC,
+    VE,
+    VG,
+    VI,
+    VN,
+    VU,
+    WF,
+    WS,
+    YE,
+    YT,
+    ZA,
+    ZM,
+    ZW
+};
